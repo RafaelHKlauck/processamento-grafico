de7a8c8fe726aa5be876bb8daa52f052c8bbb233
Rafael Hansen Klauck <rhansenklauck@gmail.com>
2024-04-28 11:56:26 -0300

    removido comentario desnecessario

--- trabalhoGA/main.cpp
+++ trabalhoGA/main.cpp
@@ -1,19 +1,9 @@
 #include "trabalhoGA/Game.h"
 
-// Fun��o MAIN
+
 int main()
 {
 	Game game = Game();
 	game.initialize();
 	game.start();
 }
-
-
-/*
-	Como mover?
-	1 - pegar sprite do personagem como variavel global e no key_callback mudar a posicao
-	
-
-	Colisão. Como fazer?
-	- armazenar altura e largura da sprite
-*/
